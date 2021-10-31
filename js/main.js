05dc57a5b6c3bbb68bc4a1fff3403233558c4bd5
SokolovaAnastasiia <aasokolova99@gmail.com>
2021-10-31 14:26:36 +0300

    layout

--- js/main.js
+++ js/main.js
@@ -7,27 +7,8 @@ $(document).ready(function(){
     $('.burger-btn').click(function(){
         $('.menu').css('display', 'flex')
     })
-
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav'
-      });
-      $('.slider-nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: true,
-        focusOnSelect: true
-      });
-     
-      $('a[data-slide]').click(function(e) {
-        e.preventDefault();
-        var slideno = $(this).data('slide');
-        $('.slider-nav').slick('slickGoTo', slideno - 1);
-      });
+  
+   
 
 
 });
